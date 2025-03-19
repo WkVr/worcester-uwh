2d14d6e204a0b73efac3c14318798c5a22d06470
wynandoctoco <135211066+wynandoctoco@users.noreply.github.com>
2025-03-19 16:04:18 +0200

    add tailwind and header

---
+++ src/components/Layout/Header.vue
@@ -0,0 +1,12 @@
+<script>
+import Logo from '@assets/Logo.png';
+</script>
+
+<template>
+	<header
+		class="flex items-center justify-center gap-4 p-4 bg-gray-800 text-white"
+	>
+		<img src="@assets/Logo.png" alt="Signature Vision" class="w-32" />
+		<h1>Signature Vision</h1>
+	</header>
+</template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+	plugins: [vue()],
+	css: {
+		postcss: './postcss.config.js',
+	},
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+			'@components': path.resolve(__dirname, 'src/components/'),
+			'@assets': path.resolve(__dirname, 'src/assets/'),
+		},
+	},
+});
