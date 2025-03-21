6939c0c69b4eea8d95e6bde8c6349605ab68f7b2
wynandoctoco <135211066+wynandoctoco@users.noreply.github.com>
2025-03-21 04:52:55 +0200

    feta: add firebase hosting

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue';
-import Header from '@components/Layout/Header.vue';
+// import HelloWorld from './components/HelloWorld.vue';
+import Header from '@components/layout/Header.vue';
 </script>
 
 <template>
 	<Header />
 	<div class="body-container">
 		<div class="container mx-auto w-full">
-			<HelloWorld msg="Signature Vision" />
+			<!-- <HelloWorld msg="Signature Vision" /> -->
 		</div>
 	</div>
 </template>
