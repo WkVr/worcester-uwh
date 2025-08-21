49a44ba13f32fb735f45c1cc31444f17002e9819
Wynand <135211066+wynandoctoco@users.noreply.github.com>
2025-08-21 14:06:45 +0200

    improve styling

--- src/components/pages/Home/Home.vue
+++ src/components/pages/Home/Home.vue
@@ -9,7 +9,7 @@ import Collage from './Collage.vue';
 		<div class="flex flex-col">
 			<div class="body-container justify-start flex-1 flex-col gap-6">
 				<div class="mt-8 p-4 flex-1 items-center justify-center">
-					<h2 class="text-center text-white">
+					<h2 class="text-center text-bold text-white">
 						Welcome to the Worcester Underwater Hockey Club
 					</h2>
 					<p class="text-center mt-4 text-white">
@@ -23,74 +23,36 @@ import Collage from './Collage.vue';
 					<Collage />
 				</div>
 			</div>
-			<div class="flex flex-col gap-6 p-4 bg-primary-light bg-opacity-50">
+			<div class="flex flex-col gap-6 p-4 bg-primary-dark bg-opacity-70">
 				<div class="body-container grid grid-cols-2">
 					<div class="">
-						<div class="width-80">
-							<h2
-								class="text-start sm:text-justify text-black text-bold w-full text-white"
-							>
-								Why Underwater Hockey?
-							</h2>
+						<div class="width-80 gap-4">
+							<h2>Why Underwater Hockey?</h2>
 							<ul>
-								<li
-									class="text-start sm:text-justify text-black w-full text-white"
-								>
-									- A unique sport that combines swimming, strategy, and
-									teamwork.
-								</li>
-								<li
-									class="text-start sm:text-justify text-black w-full text-white"
-								>
-									- Suitable for all ages and skill levels.
-								</li>
-								<li
-									class="text-start sm:text-justify text-black w-full text-white"
-								>
-									- A fun way to stay fit and make new friends.
-								</li>
-								<li
-									class="text-start sm:text-justify text-black w-full text-white"
-								>
-									- A unique sport that challenges both mind and body under the
+								<li>Suitable for all ages and skill levels.</li>
+								<li>Fun way to stay fit and make new friends.</li>
+								<li>
+									Unique sport that challenges both mind and body under the
 									water.
 								</li>
 							</ul>
-							<h2
-								class="text-start sm:text-justify text-black w-full text-white"
-							>
-								Why Underwater Hockey?
-							</h2>
-							<p
-								class="text-start sm:text-justify text-black w-full text-white"
-							>
+							<h2 class="pt-4">Who Can Join?</h2>
+							<p>
 								We welcome all ages and skill levels – from curious beginners to
 								experienced athletes. If you can swim and enjoy the water, you
 								can play! Come try a session, sharpen your skills, and
 								experience the thrill of the game. All you need is a swimsuit –
 								we’ll help with the rest!
 							</p>
-							<h2
-								class="text-start sm:text-justify text-black text-bold w-full text-white"
-							>
-								Get Involved
-							</h2>
-							<p
-								class="text-start sm:text-justify text-black w-full text-white"
-							>
+							<h2 class="pt-4">Get Involved</h2>
+							<p>
 								Come try a session, sharpen your skills, and experience the
 								thrill of the game. All you need is the spirit – we’ll help with
 								the rest!
 							</p>
-							<p
-								class="text-start sm:text-justify text-black w-full text-white"
-							>
-								📍 Based in Worcester, serving the Breede Valley and surrounding
-								communities.
-							</p>
 						</div>
 					</div>
-					<div class="md:hidden"></div>
+					<div></div>
 				</div>
 			</div>
 		</div>
